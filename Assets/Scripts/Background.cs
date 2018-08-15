ab75d700a6b9f1a4a396343e540a96f7e2559c27
mphillipsdev <mphillips.dev@gmail.com>
2018-08-15 17:20:59 -0500

    Initial commit. Includes basic player controls (unfinished), a floor, and some temporary art assets.

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    SpriteRenderer sr;
+    float worldScreenHeight;
+    float worldScreenWidth;
+    float width;
+    float height;
+
+    // Use this for initialization
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        transform.localScale = new Vector3(1, 1, 1);
+        width = sr.sprite.bounds.size.x;
+        height = sr.sprite.bounds.size.y;
+        worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+        worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+        transform.localScale = new Vector3(worldScreenWidth / width, worldScreenHeight / height, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.A))
+                moveLeft();
+            if (Input.GetKey(KeyCode.D))
+                moveRight();
+        }
+        else
+            dampen();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            jump();
+
+    }
+
+    void moveLeft()
+    {
+        Debug.Log("Moving Left");
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(-10, 0));
+    }
+
+    void moveRight()
+    {
+        Debug.Log("Moving Right");
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(10, 0));
+
+    }
+
+    void jump()
+    {
+        Debug.Log("Jump");
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 250));
+
+    }
+
+    void dampen()
+    {
+       // GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x * 0.5f, GetComponent<Rigidbody2D>().velocity.y);
+    }
+}

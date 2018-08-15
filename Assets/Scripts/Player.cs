a94b01fabb2a4c9bff9fd1ee84a17198b0991620
mphillipsdev <mphillips.dev@gmail.com>
2018-08-15 18:25:08 -0500

    Updated character controller to workable state. -MP

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,53 +4,91 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    public bool grounded = true;
+    Rigidbody2D rb;
+    float moveForce = 10.0f;
+    float jumpForce = 250.0f;
+    float driftForce = 3.0f;
+    float dragForce = 0.5f;
+    float falloffForce = 0.99f;
     // Use this for initialization
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        if (grounded)
         {
             if (Input.GetKey(KeyCode.A))
                 moveLeft();
             if (Input.GetKey(KeyCode.D))
                 moveRight();
+            if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+                drag();
+            if (Input.GetKeyDown(KeyCode.Space))
+                jump();
         }
         else
-            dampen();
+        {
+            if (Input.GetKey(KeyCode.A))
+                driftLeft();
+            if (Input.GetKey(KeyCode.D))
+                driftRight();
+            if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+                falloff();
+        }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            jump();
+
 
     }
 
     void moveLeft()
     {
-        Debug.Log("Moving Left");
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(-10, 0));
+        rb.AddForce(new Vector2(-moveForce, 0));
     }
 
     void moveRight()
     {
-        Debug.Log("Moving Right");
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(10, 0));
-
+        rb.AddForce(new Vector2(moveForce, 0));
     }
 
     void jump()
     {
-        Debug.Log("Jump");
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 250));
-
+        rb.AddForce(new Vector2(0, jumpForce));
     }
 
-    void dampen()
+    void drag()
     {
-       // GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x * 0.5f, GetComponent<Rigidbody2D>().velocity.y);
+        rb.velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x * dragForce, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void driftLeft()
+    {
+        rb.AddForce(new Vector2(-driftForce, 0));
+    }
+
+    void driftRight()
+    {
+        rb.AddForce(new Vector2(driftForce, 0));
+    }
+
+    void falloff()
+    {
+        rb.velocity = new Vector2(rb.velocity.x * falloffForce, rb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Floor")
+            grounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Floor")
+            grounded = false;
     }
 }

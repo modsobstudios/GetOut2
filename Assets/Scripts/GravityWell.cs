16f2ebead4e73ecb4d8a52fe74fd30f1ffda3871
Noel Ortiz <NOrtiz678@gmail.com>
2018-08-26 20:32:22 -0400

    Preliminary Gravity Well done and Prefab made

--- Assets/Scripts/GravityWell.cs
+++ Assets/Scripts/GravityWell.cs
@@ -8,7 +8,7 @@ public class GravityWell : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-		
+        thePlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
 	}
 	
 	// Update is called once per frame
@@ -16,4 +16,44 @@ public class GravityWell : MonoBehaviour {
     {
 		
 	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            if(thePlayer.transform.position.x > this.gameObject.transform.position.x)
+            {
+                thePlayer.GetComponent<Rigidbody2D>().AddForce(new Vector2(-100, 0));
+            }
+            else if(thePlayer.transform.position.x < this.gameObject.transform.position.x)
+            {
+                thePlayer.GetComponent<Rigidbody2D>().AddForce(new Vector2(100, 0));
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            if (thePlayer.transform.position.x > this.gameObject.transform.position.x)
+            {
+                thePlayer.GetComponent<Rigidbody2D>().AddForce(new Vector2(-15, 0));
+            }
+            else if (thePlayer.transform.position.x < this.gameObject.transform.position.x)
+            {
+                thePlayer.GetComponent<Rigidbody2D>().AddForce(new Vector2(15, 0));
+            }
+
+             if (thePlayer.transform.position.y > this.gameObject.transform.position.y)
+            {
+                thePlayer.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -15));
+            }
+            else if (thePlayer.transform.position.y < this.gameObject.transform.position.y)
+            {
+                thePlayer.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 15));
+            }
+
+        }
+    }
 }
